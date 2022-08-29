e7ddae997b5e06704ab15bf2e82b9f01bb70de24
Samyak Jain <2130153085@ipec.org.in>
2022-08-29 16:33:59 +0530

    Dark Mode for navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 // import logo from './logo.svg';
 // import './App.css';
+import React, { useState } from 'react'
 import Navbar from "./Components/Navbar";
 
 function App() {
+
+  const [mode, setMode] = useState("light");
+  const toggleMode = () =>{
+    if(mode === "light"){
+      setMode("dark");
+    }
+    else{
+      setMode("light");
+    }
+  }
+
   return (
     <>
-    <Navbar/>
+    <Navbar title="Text Utility" mode={mode} toggleMode={toggleMode}/>
     Hellow World
     </>
   );
