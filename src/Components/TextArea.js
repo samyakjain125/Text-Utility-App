2f4672198a29f4dc1aab7da0131c5cc256604b17
Samyak Jain <2130153085@ipec.org.in>
2022-08-29 20:52:07 +0530

    changed usestate to  and added placeholder

--- src/Components/TextArea.js
+++ src/Components/TextArea.js
@@ -3,7 +3,7 @@ import copy from "copy-to-clipboard";
 // import { Heading, Input1, Input2, Container, Button } from './Styles'
 
 export default function TextArea(props) {
-    const [text, setText] = useState("Enter your Text Here");
+    const [text, setText] = useState("");
     const [btnText, setBtnText] = useState("Copy Text")
     const textChanged = (event) =>{
         // console.log("text is changed");
@@ -37,7 +37,7 @@ export default function TextArea(props) {
     <div className="container">
     <div className="mb-3 my-3">
         <label htmlFor="exampleFormControlTextarea1" className={`form-label text-${props.mode ==="light"?"dark":"light"}`}>Your textarea</label>
-        <textarea className="form-control" value={text} onChange={textChanged} style={{backgroundColor: props.mode==="light"?"white":"#212529", color: props.mode==="dark"?"white":"black"}}  id="exampleFormControlTextarea1" rows="6"></textarea>
+        <textarea className="form-control" placeholder='Enter your text here' value={text} onChange={textChanged} style={{backgroundColor: props.mode==="light"?"white":"#212529", color: props.mode==="dark"?"white":"black"}}  id="exampleFormControlTextarea1" rows="6"></textarea>
         <button type="button" onClick={handleUpClick} class="btn btn-primary my-3 mx-2">Convert to UpperCase</button>
         <button type="button" onClick={handleDownClick} className="btn  btn-primary my-3 mx-2">Convert to LowerCase</button>
         <button type="button" onClick={handleClear} className="btn btn-danger my-3 mx-2">Clear</button>
