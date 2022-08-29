ad354092fbdbcda30a9f923d07a7dfb0549c9f3b
Samyak Jain <2130153085@ipec.org.in>
2022-08-29 18:13:02 +0530

    added copytoclipboard

--- src/Components/TextArea.js
+++ src/Components/TextArea.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import copy from "copy-to-clipboard";  
+// import { Heading, Input1, Input2, Container, Button } from './Styles'
 
 export default function TextArea() {
     const [text, setText] = useState("Enter your Text Here");
@@ -18,17 +20,25 @@ export default function TextArea() {
         setText(newText);
         console.log("The text is change from \n>>> "+ text + ", to \n>>> " +newText)
     }
+    const handleCopy= ()=>{
+        copy(text);
+        console.log("the text is copied")
+        alert("the text is copied")
+
+    }
     
   return (
     <div className="container">
     <div className="mb-3 my-3">
         <label htmlFor="exampleFormControlTextarea1" className="form-label">Your textarea</label>
         <textarea className="form-control" value={text} onChange={textChanged} id="exampleFormControlTextarea1" rows="6"></textarea>
+        <button type="button" onClick={handleUpClick} class="btn btn-primary my-3 mx-2">Convert to UpperCase</button>
+        <button type="button" onClick={handleDownClick} className="btn  btn-primary my-3 mx-2">Convert to LowerCase</button>
+        <button type="button" className="btn btn-primary my-3 mx-2">Primary</button>
     </div>
-        <button type="button" onClick={handleUpClick} class="btn btn-sm btn-primary my-3 mx-2">Convert to UpperCase</button>
-        <button type="button" onClick={handleDownClick} className="btn btn-sm btn-primary my-3 mx-2">Convert to LowerCase</button>
-        <button type="button" className="btn btn-sm btn-primary my-3 mx-2">Primary</button>
-        <button type="button" className="btn btn-sm btn-primary my-3 mx-2">Primary</button>
+    <h3>Your Text</h3>
+    <p>{text}</p>
+        <button type="button" onClick={handleCopy} className="btn btn-primary ">Copy Text</button>
     </div>
   )
 }
